afd3138cdb002638c13c6fdf8d91b90a8b0a036f
zadumka <90329284+zadumka@users.noreply.github.com>
2024-04-10 11:54:41 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -14,62 +14,7 @@ const authSlice = createSlice({
     error: null,
     isRefreshing: false,
   },
-  extraReducers: builder =>
-    builder
-      .addCase(register.pending, state => {
-        state.loading = true;
-      })
-      .addCase(register.fulfilled, (state, action) => {
-        state.loading = false;
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(register.rejected, state => {
-        state.loading = false;
-        state.error = true;
-      })
-      .addCase(logIn.pending, state => {
-        state.loading = true;
-      })
-      .addCase(logIn.fulfilled, (state, action) => {
-        state.loading = false;
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logIn.rejected, state => {
-        state.loading = false;
-        state.error = true;
-      })
-      .addCase(logOut.pending, state => {
-        state.loading = true;
-      })
-      .addCase(logOut.fulfilled, state => {
-        state.loading = false;
-        state.user = {
-          name: null,
-          email: null,
-        };
-        state.token = null;
-        state.isLoggedIn = false;
-      })
-      .addCase(logOut.rejected, state => {
-        state.loading = false;
-        state.error = true;
-      })
-      .addCase(refreshUser.pending, state => {
-        state.isRefreshing = true;
-      })
-      .addCase(refreshUser.fulfilled, (state, action) => {
-        state.user = action.payload;
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-      })
-      .addCase(refreshUser.rejected, state => {
-        state.isRefreshing = false;
-        state.error = true;
-      }),
-});
+ 
+
 
 export default authSlice.reducer;
